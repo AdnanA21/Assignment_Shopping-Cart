170d93da5cfa0352458047430680d81949cad201
Adnan Abdullah <adnan.abdullah660@gmail.com>
2022-04-13 17:23:37 +0600

    first commit

---
+++ src/main/java/com/example/_180041121_assignment_shopcart/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example._180041121_assignment_shopcart;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
